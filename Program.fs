8a50681f0e7bf9fa9053b95016983c72aacf1a0a
Amit Bhadra <bhadraam@vmware.com>
2021-01-05 15:45:17 -0500

    updated send and receive

--- Program.fs
+++ Program.fs
@@ -21,48 +21,77 @@ let main argv =
 
     let receivePort = 8889
     printfn "Receive port: %d" receivePort
-    
+
     let receivingClient = new UdpClient(receivePort)
     let ReceivingIpEndPoint = new IPEndPoint(IPAddress.Any, 0)
-    
+
     let sendAddress = IPAddress.Parse("192.168.10.1")
     printfn "Send address: %A" sendAddress
-    
+
     let sendPort = 8889
     printfn "Send port: %d" sendPort
-    
+
     let sendingClient = new UdpClient()
     let sendingIpEndPoint = new IPEndPoint(sendAddress, sendPort)
-    
-    let receive () = async {
+
+    let rec loop() =
+
+        let receive () = async {
             try
                 let! receiveResult = receivingClient.ReceiveAsync() |> Async.AwaitTask
                 let receiveBytes = receiveResult.Buffer
                 let returnData = Encoding.ASCII.GetString(receiveBytes)
+                //if returnData.Substring(0, 3) <> "mid" then
                 printfn "%s" returnData
             with
                 | error -> printfn "%s" error.Message }
+
+        let send () = async {
+            printfn "Send message: "
+            let (sendBytes: byte array) = Encoding.ASCII.GetBytes(Console.ReadLine())
+            try
+                sendingClient.Send(sendBytes, sendBytes.Length, sendingIpEndPoint) |> ignore
+            with
+                | error -> printfn "%s" error.Message }
+
+        receive |> ignore
+
+        let sendStatusCall () = async {
+            printfn "Send message: speed?"
+            let (sendBytes: byte array) = Encoding.ASCII.GetBytes(Console.ReadLine())
+            try
+                sendingClient.Send(sendBytes, sendBytes.Length, sendingIpEndPoint) |> ignore
+            with
+                | error -> printfn "%s" error.Message }
+
+        receive |> ignore
+            
+
+        let rec loop1() = async {
+            do! receive ()
+            return! loop1() }
+
+        let rec loop2() = async {
+            do! send ()
+            return! loop2() }
+
+        let rec loop3() = async {
+            do! sendStatusCall ()
+            Thread.Sleep 5000
+            return! loop3() }
+
+        loop1() |> Async.Start
+        //loop3() |> Async.Start
+        loop2() |> Async.RunSynchronously
         
-    let send () = async {
-        printfn "Send message: "
-        let (sendBytes: byte array) = Encoding.ASCII.GetBytes(Console.ReadLine())
-        try
-            sendingClient.Send(sendBytes, sendBytes.Length, sendingIpEndPoint) |> ignore
-        with
-            | error -> printfn "%s" error.Message }
 
-    let rec loop1() = async {
-        do! receive ()
-        return! loop1() }
+        loop()
 
-    let rec loop2() = async {
-        do! send ()
-        return! loop2() }
-
-    loop1() |> Async.Start  
-    loop2() |> Async.Start
-        
-    Console.Read() |> ignore
+    loop()
     
+
+
+    Console.Read() |> ignore
+
 
     0 // return an integer exit code
